6f8f22e06cd21037d660b64cff7d80d76ff6d280
Moataz Mohamed <Moataz.Mohamed@brmaja.com>
2022-04-21 16:16:35 +0200

    Update

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import {store, persistor} from './stores/store';
-import {PersistGate} from 'redux-persist/integration/react';
-import {Routes} from './Routes';
+import { Provider } from 'react-redux';
+import { store, persistor } from './stores/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Routes } from './Routes';
 const App = () => {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}>
-       
-      </PersistGate> */}
-      <Routes />
+      <PersistGate loading={null} persistor={persistor}>
+        <Routes />
+      </PersistGate>
     </Provider>
   );
 };
