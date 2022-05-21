70fc2cb0789cb8f0d9abc9901fb365abe090772f
Moataz0 <zo.o@hotmail.com>
2022-05-21 16:46:14 +0200

    Update

--- stores/actions/actionTypes.js
+++ stores/actions/actionTypes.js
@@ -11,8 +11,11 @@ export const CREATE_USER_SUCCESS = 'CREATE_USER_SUCCESS';
 export const CREATE_USER_FAILED = 'CREATE_USER_FAILED';
 
 export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
-
 export const SET_LOGOUT = 'SET_LOGOUT';
-
 export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
 export const SIGNUP_USER_FAIL = 'SIGNUP_USER_FAIL';
+
+export const INFO = 'info';
+export const SUCCESS = 'success';
+export const WARNING = 'warning';
+export const ERROR = 'error';
